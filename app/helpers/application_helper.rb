24005a3ff219f1869e3565cd63dbb98c4b964fba
nwfxyz <ngweifeng@u.nus.edu>
2017-07-04 08:59:47 +0000

    aasdsas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  #Returns full title if no title
+  
+  def fulltitle(page_title = '')
+    base_title = 'Ruby on Rails Tutorial Sample App'
+    if page_title.length == 0
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end

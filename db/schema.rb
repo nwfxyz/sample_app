f81f9ee90f46ca4948b8815370ff4cebfdc27a01
nwfxyz <ngweifeng@u.nus.edu>
2017-07-27 13:33:03 +0000

    db error

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20170727083231) do
-=======
-ActiveRecord::Schema.define(version: 20170714123009) do
->>>>>>> master
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
     t.string   "password_digest"
     t.string   "remember_digest"
-<<<<<<< HEAD
     t.boolean  "admin"
-=======
->>>>>>> master
+    t.string   "remember_digesta"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
